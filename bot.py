f23c534fee2bc1ecf7e7fdb5900cd2e847d66caa
oxoxoekb <oxoxo.ekb@gmail.com>
2021-09-16 21:42:26 +0500

    Добавлены несколько кубиков, пока без кнопок

--- bot.py
+++ bot.py
@@ -9,21 +9,39 @@
 # CommandHandler - позволяет описывать команды
 # MessageHandler - класс для обработки сообщений
 # Filters - класс для различного рода фильтров
-
+import random
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-# Функция для вызова бота на работу командой "/startuem"
+
+# Функция для вызова бота на работу командой "/start"
+
 def start(update, context):
-    update.message.reply_text('Да работаю я, работаю -_-')
+    first_name = update.message.chat.first_name
+    update.message.reply_text(f"Превед, {first_name}. Какой кубик бросим?")
+
+# Рандом
+def rndm1(update, context):
+    dice=[0,1]
+    update.message.reply_text(f"Выпало значение: {random.choice(dice)}")
+def rndm6(update, context):
+    dice=[1,2,3,4,5,6]
+    update.message.reply_text(f"Выпало значение: {random.choice(dice)}")
+def rndm20(update, context):
+    dice=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    update.message.reply_text(f"Выпало значение: {random.choice(dice)}")
+
 # Функция вызова помощи командой "/help"
 def help(update, context):
-    update.message.reply_text('Нехочунебуду -_-, но вообще можешь написать /startuem')
+    update.message.reply_text('/start - чтобы стартануть, /1d1,/1d6,/1d20 - выбор кубика')
+
 # Функция передачи ошибок в dispatcher
 def error(update, context):
     update.message.reply_text('Ошибка, ошибка -_-')
+
 # Функция обработки обычного текста
 def text(update, context):
     text_received = update.message.text
-    update.message.reply_text(f'Зачем ты мне написал: "{text_received}" ?')
+    update.message.reply_text(f'Моя твоя не понимайт. Ты написал "{text_received}", а нужно выбрать команду из /help')
+
 def main():
     # Создаем updater и dispatcher для диалога
     # Прячем токен =)
@@ -33,8 +51,11 @@ def main():
     updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     # Добавляем обработчики в dispatcher для старта и помощи.
-    dispatcher.add_handler(CommandHandler("startuem", start))
+    dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("1d1", rndm1))
+    dispatcher.add_handler(CommandHandler("1d6", rndm6))
+    dispatcher.add_handler(CommandHandler("1d20", rndm20))
     # Обработчик для обычного текста
     dispatcher.add_handler(MessageHandler(Filters.text, text))
     # Обработчик для ошибок (зачем он мне?)
